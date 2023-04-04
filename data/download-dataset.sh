050ce0900c39ba681360aa7d36ff716d7f867982
Srivarshan <srivarshan02@gmail.com>
2023-04-04 10:22:45 +0530

    Minor change: execute for dir

--- data/download-dataset.sh
+++ data/download-dataset.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-rm ./data/arg_quality_rank_30k.csv
+if [ -f arg_quality_rank_30k.csv ]
+then
+    rm arg_quality_rank_30k.csv
+fi
 wget "https://www.research.ibm.com/haifa/dept/vst/files/IBM_Debater_(R)_arg_quality_rank_30k.zip"
 unzip *.zip
 rm *.zip
 rm readme.txt
-mv *.csv ./data/.
