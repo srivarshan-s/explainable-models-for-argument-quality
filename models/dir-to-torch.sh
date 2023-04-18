b15da0a46c224e54670a49bc140341b74c1b0a73
Srivarshan <srivarshan02@gmail.com>
2023-04-19 01:13:55 +0530

    Restructure the saved regression models

--- models/dir-to-torch.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-for bertmodel in bert-*/
-do
-    bertmodel_name="${bertmodel:0:-1}.pt"
-    echo "Compressing ${bertmodel} to ${bertmodel_name}"
-    zip -rq $bertmodel_name $bertmodel
-done
-
